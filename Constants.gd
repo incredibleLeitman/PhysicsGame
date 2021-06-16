019d3ee801d5da9136bdddb8b6edf419469fa84f
incredibleLeitman <michael.leithner@gmail.com>
2021-06-16 13:25:32 +0200

    fixed gravity values to use pixel/s instead of m/s2

--- Constants.gd
+++ Constants.gd
@@ -2,7 +2,7 @@ extends Node
 
 
 # player movement
-const GRAVITY := 9.81
+const GRAVITY := 2000 #9.81 # this hurts a little bit but in 2D gravity is represented in pixels/s
 const MOVE_DRAG := 0.2
 const MOVE_ACCELERATION := Vector2(200, 20)
 
