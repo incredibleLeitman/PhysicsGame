9dbd09575a9185af9ffefa99f7ec81df9054b163
incredibleLeitman <michael.leithner@gmail.com>
2021-06-16 13:27:22 +0200

    use base class application for forces

--- Pawn/Enemy/Enemy.gd
+++ Pawn/Enemy/Enemy.gd
@@ -2,10 +2,16 @@
 extends Pawn
 
 
-# TODO: move to Pawn BaseClass
 func _ready() -> void:
+	_velocity.x = -MAX_SPEED.x/2
 	set_physics_process(false)
 
 
 func _physics_process(delta: float) -> void:
-	pass
+
+	if not is_on_floor():
+		apply_gravity(delta)
+
+	if is_on_wall():
+		_velocity.x *= -1
+	_velocity.y = move_and_slide(_velocity, Vector2.UP).y
--- Pawn/Pawn.gd
+++ Pawn/Pawn.gd
@@ -10,19 +10,9 @@ var ground_velocity := Vector2.ZERO # velocity for ground, platform, etc...
 
 export var MAX_SPEED := Vector2(500, 2000)
 
-func _physics_process(delta: float) -> void:
 
-	# TODO: FIXME
-	return
-
-	# adapt velocity from ground
-	_velocity = lerp(_velocity, ground_velocity, Constants.MOVE_DRAG)
-
-	# apply forces like gravity
+func apply_gravity(delta: float) -> void:
 	_velocity.y += Constants.GRAVITY * _mass * delta
-	
-	# set max speed
-	_velocity.x = clamp(_velocity.x, -MAX_SPEED.x, MAX_SPEED.x)
-	_velocity.y = clamp(_velocity.y, -MAX_SPEED.y, MAX_SPEED.y)
 
-	_velocity = move_and_slide(_velocity, Vector2.UP)
+func add_force(force: Vector2) -> void:
+	_velocity = force
