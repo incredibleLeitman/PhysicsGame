560b95535f972d0f9ab8ed1a3071bd56e57657bb
incredibleLeitman <michael.leithner@gmail.com>
2021-06-23 14:36:29 +0200

    use suvat and quadratic equation to resolve angle for cannons

--- Pawn/Enemy/Cannon.gd
+++ Pawn/Enemy/Cannon.gd
@@ -3,36 +3,91 @@ extends KinematicBody2D
 export(NodePath) var player_node
 onready var player = get_node(player_node)
 
-export var delay := 3.0 # time between shots
+export var delay := 6.0 # time between shots
+export var speed = 1000.0 # projectile speed
+export var g := Constants.GRAVITY
 
-var bullet_scene = preload("res://Pawn/Enemy/Bomb.tscn")
+onready var bomb_scene = preload("res://Pawn/Enemy/Bomb.tscn")
+onready var aim_curve = $Line2D
+onready var v2 = speed * speed
 
-var _rotation := 0.0
+var _velocity := Vector2.ZERO
 var _reset := 0.0
 
 
 func _ready() -> void:
 	set_physics_process(false)
 
+	#if name == "Cannon":
+	#	for deg in range(0, 361, 5):
+	#		var rad = deg2rad(deg)
+	#		print("deg: ", deg, " in rad: ", rad)
+	#		print("	rad: -> tan: ", tan(rad), " -> atan: ", atan(tan(rad)), " back to grad: ", rad2deg(atan(tan(rad))))
 
 func _physics_process(delta: float) -> void:
-	_reset += delta
-	if _reset > delay:
-		shoot()
-	elif _reset > 1.0:
-		$CollisionShapeTop.rotate(_rotation + delta)
 
+	aim_curve.clear_points()
 
+	# target player
+	var pos = player.global_position
+	if abs(pos.x - $CollisionShapeBase.global_position.x) > 200 or abs(pos.y - $CollisionShapeBase.global_position.y) > 400 :
 
-func shoot() -> void:
+		var x = pos.x - $CollisionShapeTop/SpriteCrosshair.global_position.x
+		var y = -pos.y - $CollisionShapeTop/SpriteCrosshair.global_position.y
+
+		# solve quadratic equation for theta
+		# shamelessly stolen from the great Alex Rose who used this in RudeBear
+		# "Physics for Unity - 22: SUVAT Equations and 3D Projectile Prediction"
+		# I tried to derive the formula myself but failed miserably -.-
+		var quotient = v2*v2 - g*g * x*x - 2 * v2*y*g
+		if quotient > 0:
+
+			_reset += delta
+
+			var result = (v2 - sqrt(quotient)) / (g*x)
+			var angle = atan(result)
+
+			_velocity = Vector2(
+				(1 if x > 0 else -1) * speed * cos(angle),
+				(1 if x > 0 else -1) * speed * sin(angle)
+			)
+			
+			_draw_curve()
+
+			$CollisionShapeTop.rotation = angle + (PI if x < 0 else 0)
+
+			if _reset > delay:
+				_shoot()
+
+			return
+
+	# if player is in close-range or cannot be reached continue rotating
+	$CollisionShapeTop.rotation += delta
 	_reset = 0
 
-	var pos = player.get_position()
+func _draw_curve() -> void:
 
-	# look at current player position
-	$CollisionShapeTop.look_at(pos)
+	aim_curve.global_position = $CollisionShapeTop/SpriteCrosshair.global_position
+	for idx in range(0, 500):
 
-	var instance = bullet_scene.instance()
-	instance.position = $CollisionShapeTop/icon.global_position
-	instance.initial_velocity.x *= sign(pos.x - self.position.x)
-	get_tree().get_root().add_child(instance)
+		var t = idx * get_physics_process_delta_time()
+
+		var sx = _velocity.x * t
+		var sy = _velocity.y * t + 0.5 * g * t*t
+
+		aim_curve.add_point(Vector2(sx, sy))
+
+func _shoot() -> void:
+
+	var instance = bomb_scene.instance()
+	instance.position = $CollisionShapeTop/SpriteCrosshair.global_position
+	instance.initial_velocity = _velocity
+	#get_tree().get_root().add_child(instance)
+	get_parent().add_child(instance)
+	#instance.owner = self
+	#add_child(instance)
+
+	_reset = 0
+
+func _on_VisibilityEnabler2D_screen_exited() -> void:
+	aim_curve.clear_points()
