20514f312a878fb50fbb59545b5646bd2002e13d
incredibleLeitman <michael.leithner@gmail.com>
2021-06-23 14:13:04 +0200

    calculate platform movement in physics process instead of using animation player

--- Level/Platform.gd
+++ Level/Platform.gd
@@ -1,8 +1,31 @@
-extends Node2D
+extends KinematicBody2D
 
+export var speed := 150.0
+export var mode := 0 # TODO enum hor, ver, diag, sin...
+export var distance := 300
 
-export var animation := "stop"
-
+var _start_pos = Vector2.ZERO
+var _dir := Vector2.ZERO
 
 func _ready() -> void:
-	$AnimationPlayer.current_animation = animation
+	_start_pos = position
+	if mode == 0:
+		_dir = Vector2(sign(distance), 0)
+	elif mode == 1:
+		_dir = Vector2(0, sign(distance))
+
+func _physics_process(delta: float) -> void:
+	position += speed * _dir * delta
+	if mode == 0:
+		if position.x > _start_pos.x + distance:
+			_dir = Vector2(-1, 0)
+		elif position.x < _start_pos.x - distance:
+			_dir = Vector2(+1, 0)
+	elif mode == 1:
+		if position.y > _start_pos.y + abs(distance):
+			_dir = Vector2(0, -1)
+		elif position.y < _start_pos.y - abs(distance):
+			_dir = Vector2(0, +1)
+
+func get_ground_velocity () -> Vector2:
+	return speed * _dir
